229d93b980217e1297fc65df966c0dff2d742285
noumanhafeez <noumanhafeez095@gmail.com>
2026-03-06 16:30:49 +0500

    update n-gram for pipeline

--- source/n-grams.py
+++ source/n-grams.py
@@ -1,30 +1,46 @@
-from preprocessing import tokens
+# n_grams.py
+from collections import Counter
+
+def get_unigram(tokens):
+    """
+    Return list of unigrams (single tokens).
+    """
+    return tokens
 
 
-def unigram(content):
-    unigram = content
-    return unigram
+def get_bigrams(tokens):
+    """
+    Return list of bigrams(2 words) as tuples.
+    """
 
-
-def bigrams(content):
     bigrams = []
-
-    for i in range(len(content) - 1):
-        bigrams.append((content[i], content[i + 1]))
+    for i in range(len(tokens) - 1):
+        bigrams.append((tokens[i], tokens[i + 1]))
     return bigrams
 
 
-def trigrams(content):
+def get_trigrams(tokens):
+    """
+    Return list of trigrams (3 words) as tuples.
+    """
+
     trigrams = []
-    for i in range(len(content) - 2):
-        trigrams.append((content[i], content[i + 1], content[i + 2]))
+    for i in range(len(tokens) - 2):
+        trigrams.append((tokens[i], tokens[i + 1], tokens[i + 2]))
     return trigrams
 
 
-uni = unigram(tokens)
-print(uni)
+def get_ngram_frequencies(tokens):
+    """
+    Return frequency counters for uni, bi, and trigrams.
+    """
 
-bi = bigrams(tokens)
-#print(bi)
-trig = trigrams(tokens)
-#print(trig)
+    unigrams = get_unigram(tokens)
+    bigrams = get_bigrams(tokens)
+    trigrams = get_trigrams(tokens)
+
+    uni_freq = Counter(unigrams)
+    bi_freq = Counter(bigrams)
+    tri_freq = Counter(trigrams)
+
+    return uni_freq, bi_freq, tri_freq
